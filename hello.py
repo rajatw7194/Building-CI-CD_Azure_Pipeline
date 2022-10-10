e74e6fc1a2f4a72911b59b40e47e207d7281c58a
rajatw7194 <75608128+rajatw7194@users.noreply.github.com>
2022-10-10 23:28:05 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,12 +4,12 @@ import click
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
-    if name == "Thor":
-        click.echo("Thor, you are always red.")
-        click.echo(click.style(f"Hello {name}!", fg="red"))
-    else:
-        click.echo(f"Your color is {color}!")
-        click.echo(click.style(f"Hello {name}!", fg=color))
+  if name == "Thor":
+    click.echo("Thor, you are always red.")
+    click.echo(click.style(f"Hello {name}!", fg="red"))
+  else:
+    click.echo(f"Your color is {color}!")
+    click.echo(click.style(f"Hello {name}!", fg=color))
 
 if __name__ == "__main__":
-    hello()
+  hello()
