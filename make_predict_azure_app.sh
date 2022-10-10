e7a625167b963fb10323b3c61206b7176556692a
rajatw7194 <75608128+rajatw7194@users.noreply.github.com>
2022-10-11 00:53:00 +0530

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
-     #your application name <yourappname>goes here
+     -X POST https://flaskwebapp.azurewebsites.net:$PORT/predict 
+     #your application name flaskwebapp goes here
